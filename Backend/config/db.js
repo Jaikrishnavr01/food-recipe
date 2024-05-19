04dd93ad56bf48d9e2983363206387ba5f2a7a3a
jaikrishnavr01 <jaikrishnavr@outlook.com>
2024-05-19 11:52:25 +0530

    signup email verifcation

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connetDB =() => {
-    mongoose.connect("mongodb://127.0.0.1:27017/Recipe")
+    mongoose.connect(process.env.DB_URI)
 
     mongoose.connection.on('connected',() => {
         console.log("----monogodb server conneted with backend----");
--- Backend/controllers/Auth_Controller.js
+++ Backend/controllers/Auth_Controller.js
@@ -1,30 +1,59 @@
 const userModel = require('../models/User_model')
-const {v4:uuidv4} = require('uuid')
+const { v4: uuidv4 } = require('uuid')
 const bcrypt = require('bcryptjs')
 const nodemailer = require('nodemailer')
 
-exports.signup = async (req, res)=>{
-    const {email, password, username} = req.body
+exports.signup = async (req, res) => {
+    const { email, password, username } = req.body
 
     let user = await userModel.findOne({ email });
-    if(user){
-        return res.status(400).json({message:"email already registered"})
+    if (user) {
+        return res.status(400).json({ message: "email already registered" })
     }
 
     //password encrypt
     const salt = await bcrypt.genSalt(10)
     const hashedPassword = await bcrypt.hash(password, salt)
-    
+
     // generate random activation code
     const activationCode = uuidv4()
-    
-    user =  new userModel({
+
+    user = new userModel({
         username,
         email,
-        password:hashedPassword,
+        password: hashedPassword,
         activationCode
     })
 
     await user.save()
-    res.status(200).json({message:"user created successfully"})
+    res.status(200).json({ message: "user created successfully" })
+
+    // email sending 
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com', //smtp.office365.com  --> outlook, smtp.gmail.com ---> gmail 
+        port: 587, // 587 --> for outlook / gmail , 465 -> hostinger
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD
+        }
+    })
+
+    // mail activation link
+    const activationLink = `http://localhost:${process.env.PORT}/auth/activate/${activationCode}`
+
+    const mailOption = {
+        from: process.env.EMAIL_USER,
+        to: email,
+        subject: 'Account activation link',
+        text: `Please click on the link to activate your account ${activationLink}`
+    }
+    transporter.sendMail(mailOption, (err, info) => {
+        if (err) {
+            console.log(err)
+            return res.status(500).json({ message: "cannot email activation link is sent" })
+        } else {
+            console.log(info)
+            return res.status(200).json({ message: "email activation link is sent" })
+        }
+    })
 }
